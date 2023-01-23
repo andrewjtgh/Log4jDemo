62332eeaac228625580f55cf14503f35ed7e958a
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-01-23 20:59:47 +0800

    Final passing

--- Log4jDemo/test/tips/java/demo/Log4jDemoTest.java
+++ Log4jDemo/test/tips/java/demo/Log4jDemoTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Log4jDemoTest {
-	 @Test
-	    void testEquals() {
+	@Test
+	    public void testEquals() {
 		 assertEquals(1,1);
+		 return;
 	 }
 }
